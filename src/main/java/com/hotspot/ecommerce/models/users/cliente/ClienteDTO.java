697c998b3406cefd16afed368be43a5daa90ba86
Azzasp <p.erickbatista@gmail.com>
2022-11-29 20:45:50 -0300

    Cliente DTO

--- src/main/java/com/hotspot/ecommerce/models/users/cliente/ClienteDTO.java
+++ src/main/java/com/hotspot/ecommerce/models/users/cliente/ClienteDTO.java
@@ -43,4 +43,76 @@ public class ClienteDTO {
         this.endereco = cliente.getEndereco();
         this.formatoPagamento = cliente.getFormatoPagamento();
     }
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public Date getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(Date data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public FormatoPagamentoPreferencial getFormatoPagamento() {
+        return formatoPagamento;
+    }
+
+    public void setFormatoPagamento(FormatoPagamentoPreferencial formatoPagamento) {
+        this.formatoPagamento = formatoPagamento;
+    }
 }
