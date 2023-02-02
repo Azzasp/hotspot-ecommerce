9163cbf76cbe0a5ae457c838a54146deb2a9305b
Azzasp <p.erickbatista@gmail.com>
2023-02-02 10:27:24 -0300

    refactoring ImageController methods inputs

--- src/main/java/com/hotspot/ecommerce/models/mercadorias/imagem/ImagemController.java
+++ src/main/java/com/hotspot/ecommerce/models/mercadorias/imagem/ImagemController.java
@@ -17,37 +17,39 @@ public class ImagemController {
     private final ImagemService imagemService;
 
 
-    @GetMapping("/produto/{id_produto}")
-    public ResponseEntity<List<ImagemProduto>> findImageByProdutoId(@PathVariable Long id_produto){
+    @GetMapping("/produto")
+    public ResponseEntity<List<ImagemProduto>> findImageByProdutoId(@RequestParam Long id_produto){
         return imagemService.findImageByProdutoId(id_produto);
     }
 
     @DeleteMapping("/produto/{id_produto}/{id_imagem}")
-    public ResponseEntity<ImagemProduto> deleteImageByProdutoAndImageId(@PathVariable Long id_produto,
-                                                                         @PathVariable Long id_imagem){
+    public ResponseEntity<ImagemProduto> deleteImageByProdutoAndImageId(@PathVariable("id_produto") Long id_produto,
+                                                                         @PathVariable("id_imagem") Long id_imagem){
         return imagemService.deleteImageByProdutoAndImageId(id_produto, id_imagem);
     }
 
-    @PostMapping("/produto/{id_produto}")
-    public ResponseEntity<ImagemProduto> insertProdutoImage(Long id_produto, MultipartFile file){
+    @PostMapping("/produto")
+    public ResponseEntity<ImagemProduto> insertProdutoImage(@RequestParam("id_produto") Long id_produto,
+                                                            @RequestParam("file") MultipartFile file){
         return imagemService.insertProdutoImage(id_produto, file);
     }
 
 
     //Serviços
-    @GetMapping("/servico/{id_servico}")
-    public ResponseEntity<List<ImagemServico>> findImageByServicoId(@PathVariable Long id_servico){
+    @GetMapping("/servico")
+    public ResponseEntity<List<ImagemServico>> findImageByServicoId(@RequestParam Long id_servico){
         return imagemService.findImageByServicoId(id_servico);
     }
 
     @DeleteMapping("/servico/{id_servico}/{id_imagem}")
-    public ResponseEntity<ImagemServico> deleteImageByServicoAndImageId(@PathVariable Long id_servico,
-                                                                        @PathVariable Long id_imagem){
+    public ResponseEntity<ImagemServico> deleteImageByServicoAndImageId(@PathVariable("id_servico") Long id_servico,
+                                                                        @PathVariable("id_imagem") Long id_imagem){
         return imagemService.deleteImageByServicoAndImageId(id_servico, id_imagem);
     }
 
-    @PostMapping("/servico/{id_produto}")
-    public ResponseEntity<ImagemServico> insertServicoImage(Long id_servico, MultipartFile file){
+    @PostMapping("/servico")
+    public ResponseEntity<ImagemServico> insertServicoImage(@RequestParam("id_service") Long id_servico,
+                                                            @RequestParam("file") MultipartFile file){
         return imagemService.insertServicoImage(id_servico, file);
     }
 }
